01bacf9b684341d4705852d9e214a0be522bd3f0
Jakob Runge <github@runjak.codes>
2023-06-11 13:32:56 +0200

    try nodejs mainV2 with deno

--- nodejs/mainV2.js
+++ nodejs/mainV2.js
@@ -1,19 +1,24 @@
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+const delay = () => new Promise(resolve => setTimeout(resolve, 10_000))
 
 const runTasks = (numTasks) => {
   const tasks = new Array(numTasks);
 
   for (let i = 0; i < numTasks; i++) {
-    tasks[i] = delay(10000);
+    tasks[i] = delay();
   }
 
   return tasks;
 }
 
-const numTasks = parseInt(process.argv[2]) || 10000;
+const numTasks = 1_000_000 // parseInt(process.argv[2]) || 10000;
 
 Promise.all(runTasks(numTasks)).then(() => {
   console.log('All tasks completed');
 }).catch((error) => {
   console.error('Error:', error);
 });
+
+// /usr/bin/time -f "%M" node ./mainV2.js 
+// 506036
+// /usr/bin/time -f "%M" deno run ./mainV2.js
+// 1M: > 30m and continuingee
